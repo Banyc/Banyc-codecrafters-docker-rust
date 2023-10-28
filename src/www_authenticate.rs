a03300d93bac72ef36c567c97c64208df80b3366
Banyc <36535895+Banyc@users.noreply.github.com>
2023-10-28 18:07:18 +0800

    feat: www-authenticate parser

--- src/www_authenticate.rs
+++ src/www_authenticate.rs
@@ -1,6 +1,74 @@
-use chumsky::prelude::*;
+use std::{collections::HashMap, str::FromStr};
 
-#[derive(Debug, Clone, PartialEq)]
+use chumsky::prelude::*;
+use getset::Getters;
+use once_cell::sync::Lazy;
+
+#[allow(clippy::type_complexity)]
+static LEXER: Lazy<Box<dyn Parser<char, Vec<Token>, Error = Simple<char>> + Sync + Send>> =
+    Lazy::new(|| Box::new(lexer()));
+static PARSER: Lazy<Box<dyn Parser<Token, WwwAuthenticate, Error = Simple<Token>> + Sync + Send>> =
+    Lazy::new(|| Box::new(parser()));
+
+#[derive(Debug, Getters)]
+pub struct WwwAuthenticate {
+    #[getset(get = "pub")]
+    scheme: String,
+    #[getset(get = "pub")]
+    pairs: HashMap<String, String>,
+}
+
+impl FromStr for WwwAuthenticate {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tokens = LEXER.parse(s).map_err(|_| ())?;
+        PARSER.parse(tokens).map_err(|_| ())
+    }
+}
+
+fn parser() -> impl Parser<Token, WwwAuthenticate, Error = Simple<Token>> {
+    // scheme <- key
+    let scheme = filter(|token| matches!(token, Token::Key(_))).map(|token| {
+        if let Token::Key(key) = token {
+            return key;
+        }
+        panic!()
+    });
+
+    // pair <- key eq value
+    let pair = filter(|token| matches!(token, Token::Key(_)))
+        .map(|token| {
+            if let Token::Key(key) = token {
+                return key;
+            }
+            panic!()
+        })
+        .then(just(Token::Eq).ignored())
+        .then(
+            filter(|token| matches!(token, Token::Value(_))).map(|token| {
+                if let Token::Value(value) = token {
+                    return value;
+                }
+                panic!()
+            }),
+        )
+        .map(|((key, _), value)| (key, value));
+
+    // s <- scheme ( pair )comma* $
+    scheme
+        .then(pair.separated_by(just(Token::Comma)))
+        .then_ignore(just(Token::End))
+        .map(|(scheme, pairs)| {
+            let mut map = HashMap::new();
+            pairs.into_iter().for_each(|(key, value)| {
+                map.insert(key, value);
+            });
+            WwwAuthenticate { scheme, pairs: map }
+        })
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, std::hash::Hash)]
 enum Token {
     Eq,
     Comma,
@@ -53,5 +121,17 @@ mod tests {
                 Token::End,
             ]
         );
+    }
+
+    #[test]
+    fn test_parser() {
+        let parser = parser();
+        let lexer = lexer();
+        let src = r#"Bearer a="b", c="d" "#;
+        let tokens = lexer.parse(src).unwrap();
+        let ast = parser.parse(tokens).unwrap();
+        assert_eq!(ast.scheme, "Bearer");
+        assert_eq!(ast.pairs.get("a").unwrap(), "b");
+        assert_eq!(ast.pairs.get("c").unwrap(), "d");
     }
 }
