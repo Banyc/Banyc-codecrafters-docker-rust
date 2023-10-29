c5b3cab4ed8e2c378a97c9a36a4fdb01db83e98d
Banyc <36535895+Banyc@users.noreply.github.com>
2023-10-29 17:44:30 +0800

    feat: interactive command

--- src/main.rs
+++ src/main.rs
@@ -60,24 +60,23 @@ fn main() -> Result<()> {
     }
 
     // Execute the command
-    let output = std::process::Command::new(command)
+    let exit_status = std::process::Command::new(command)
         .args(command_args)
-        .output()
+        .stdin(std::process::Stdio::inherit())
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{:?}' with arguments {:?}",
                 command, command_args
             )
-        })?;
-
-    // Redirect outputs
-    let std_out = std::str::from_utf8(&output.stdout)?;
-    print!("{std_out}");
-    let std_err = std::str::from_utf8(&output.stderr)?;
-    eprint!("{std_err}");
+        })?
+        .wait()
+        .unwrap();
 
     // Return exit code
-    if let Some(code) = output.status.code() {
+    if let Some(code) = exit_status.code() {
         std::process::exit(code);
     }
     Ok(())
